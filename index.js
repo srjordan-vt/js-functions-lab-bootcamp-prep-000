80d298dd4286eac755b160f6a238e454e6c6ce63
Sara Jordan <sara.jordan@gmail.com>
2018-01-15 13:20:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function(happyHolidays){
+  console.log('Happy Holidays!')
+}
+happyHolidays()
+
+function(happyHolidaysTo){
+  console.log('Happy Holidays, ${cat}')
+}
+happyHolidaysTo()
+function(happyHolidayTo){
+  console.log('Happy ${Easter}, ${bunny}!')
+}
+happyHolidayTo()
+function(holidayCountdown){
+  console.log("it's ${30} days until ${Valendtines Day}!)")
+}
